2c24b528c1e5adeab8f4a7ea6ca420fb23c46ac1
Ali Aliyev <147966223+aliiyevali@users.noreply.github.com>
2023-11-07 13:28:19 -0500

    Add files via upload

--- MoviesSQL.sql
+++ MoviesSQL.sql
@@ -1,4 +1,6 @@
 use proje;
+
+# Conditional join 
 LOAD DATA Local INFILE "/Users/aa085/Lets begin/AllSql.csv" INTO table allsql5 FIELDS TERMINATED BY ','ENCLOSED BY '"';
 select * from allsql5;
 CREATE TABLE allsql5 (
@@ -22,6 +24,7 @@ SELECT
 FROM allsql5
 LEFT JOIN repairsql2 ON allsql5.numer = repairsql2.ID;
 
+# Full Join 
 Create TABLE Adv (
 	ID INT PRIMARY KEY,
     Name VARCHAR(255),
@@ -42,3 +45,10 @@ Select A.Name,A.Worldwide,A.Adventure,Fantasy.Fantasy
 from Adv as A
 cross join  
 Fantasy on Fantasy.Name=A.Name;
+
+#Checking Successfully inserted dataset in SQL
+
+Select * from Movio;
+
+
+
